81d046f2f31616a63d169006d0735b094e7b82b0
Riley Grant <grantrileyh@gmail.com>
2022-04-08 10:49:50 -0500

    Add blank notebook, structure, cigarette data files

---
+++ data/clean_cigarette_data.py
@@ -0,0 +1,128 @@
+# to be used for data cleaning
+import numpy as np
+import pandas as pd
+import matplotlib as matplotlib
+
+import json
+
+
+
+
+# import the cigarette data
+cig_df = pd.read_csv('data/cigarette_data.csv')
+
+# putting this bad larry as a variable so we can change which year
+#   if we do correlation stuff later on in the process
+year_to_keep = 2005
+
+
+# === clean data 
+
+# clean the cigarette dataframe
+
+# drop irrelevant rows
+cig_df = cig_df.dropna(subset=['county']) # drop all 'NaN' counties
+cig_df = cig_df[cig_df.sex == 'Both'] # keep only 'both' sexes row
+cig_df = cig_df[cig_df.year == year_to_keep] # keep only one year
+
+# rename counties for consistenty, drop any suffixes (__ County, __ Borough, __ City, ..., etc)
+cig_df['county_short'] = cig_df['county']
+cig_df.loc[cig_df['county'].str.endswith(" County"), 'county_short'] = cig_df['county'].str.removesuffix(" County") # drop county, works for most
+cig_df.loc[cig_df['county'].str.endswith(" Parish"), 'county_short'] = cig_df['county'].str.removesuffix(" Parish") # drop 'Parish', for Louisiana
+cig_df.loc[cig_df['county'].str.endswith(" City"), 'county_short'] = cig_df['county'].str.removesuffix(" City") # drop 'City', for a few states. More consistent to drop it all the time.
+
+# alaska shiz - so inconsistent
+cig_df.loc[cig_df['county'].str.endswith(" Borough"), 'county_short'] = cig_df['county'].str.removesuffix(" Borough") 
+cig_df.loc[cig_df['county'].str.endswith(" Census Area"), 'county_short'] = cig_df['county'].str.removesuffix(" Census Area") 
+# cig_df.loc[cig_df['county'].str.endswith(" City"), 'county_short'] = cig_df['county'].str.removesuffix(" City")
+cig_df.loc[cig_df['county'].str.endswith(" Municipality"), 'county_short'] = cig_df['county'].str.removesuffix(" Municipality") 
+
+# of note:
+# - alaska is WEIRD not consistent, lots of specific suffixes to drop
+# - suffix of 'city' is inconsistent
+#   - i.e., cigarette data has Harrisonburg City as a county name, hdByCounty has Harrisonburg as the county
+#   -       cigarette data and hdByCounty BOTH have 'Richmond City' as a county name
+#   -       I opted to drop city, as there werent' many counties_city in hdByCounty, but some data will be lost in the join
+
+
+
+
+# === rename states to country code, add a column
+# import json
+sta_json = open('data/states_to_abbrev.json')
+states_to_abbrev_dict = json.load(sta_json)
+
+cig_df['state_abbrev'] = cig_df['state']
+cig_df['state_abbrev'] = cig_df.loc[:, 'state']
+
+
+cig_df = cig_df.replace( {
+    'state_abbrev': states_to_abbrev_dict
+})
+
+
+
+# # create single column for joining
+# cig_df['state_county'] = cig_df['county']
+cig_df['county_state'] = cig_df.loc[:, 'county_short'] + ', ' + cig_df.loc[:, 'state_abbrev']
+
+
+
+# rename column headers - make sure they're readable as cigarette data
+cig_df.rename(columns = {
+    'state'      : 'state_full',
+    'county'     : 'county_full',
+    'sex'        : 'cig_sex',
+    'year'       : 'cig_year',
+    'total_mean' : 'cig_total',
+    'total_lb'   : 'cig_total_lb',
+    'total_ub'   : 'cig_total_ub',
+    'daily_mean' : 'cig_daily',
+    'daily_lb'   : 'cig_daily_lb',
+    'daily_ub'   : 'cig_daily_ub',
+}, inplace = True)
+
+
+# re-order the whole bad larry
+cig_df = cig_df[ 
+    [
+        'county_state', 'county_short', 'state_abbrev',
+        'cig_total', 'cig_daily',
+        'county_full', 'state_full',
+        'cig_sex', 'cig_year',
+        'cig_total_lb', 'cig_total_ub', 'cig_daily_lb', 'cig_daily_ub',
+    ] 
+]
+
+# drop the old index column
+# cig_df.drop(cig_df.columns[0], axis=1, inplace=True) # drop the old index
+cig_df.reset_index(drop=True, inplace=True)
+
+# OPTIONAL - remove all unneeded columns? just keep the total_mean, daily_mean
+cig_df.drop(['cig_sex', 'cig_year'], axis=1, inplace=True) # drop sex and year, irrelevant now
+# cig_df.drop(['cig_sex'], axis=1, inplace=True) # drop sex and year, irrelevant now
+cig_df.drop(['cig_total_lb', 'cig_total_ub', 'cig_daily_lb', 'cig_daily_ub'], axis=1, inplace=True) # drop upper and lower bounds, only keep mean
+
+# print(cig_df.shape[0])
+# count = 0
+# for row_index in range(cig_df.shape[0]):
+#     this_row = cig_df.iloc[row_index]
+#     print(this_row)
+
+
+#     count += 1
+
+# print("count:", count)
+
+# cig_df['test'] = cig_df.groupby(['county_state'])['cig_year'].transform(lambda x : ''.join(str(x)))
+# cig_df_out = cig_df.set_index(['county_state', cig_df.groupby(['county_state']).cumcount()+1]).unstack().sort_index(level=1, axis=1)
+
+
+
+# print out the head
+# print(cig_df.head())
+print(cig_df)
+# print(cig_df_out)
+
+# optional - save it as a csv
+cig_df.to_csv('data/cleaned_cigarette_data.csv', index = False)
